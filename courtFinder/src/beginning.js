5c7ee2ad36dc06d108fd03847a52137ed985a5b5
Holden Eagle <holden.eagle@gmail.com>
2024-07-23 17:08:16 -0400

    adding front page elements

--- courtFinder/src/beginning.js
+++ courtFinder/src/beginning.js
@@ -3,6 +3,23 @@ import "./introImage.css"
 
 const IntroImage = () => {
     const [currentPage , setPage] = useState('IntroPage');
+    const [currentLocation , setCurrent] = useState(false);
+    const [addressText , setAddress] = useState('');
+    const [distance , setDistance] = useState(0);
+
+    const useCurrent = () => {
+        setCurrent(true);
+    }
+
+    const changeAddress = (e) => {
+        const {name , value} = e.target;
+        setAddress(value);
+    }
+
+    const changeDistance = (e) => {
+        const {name , value} = e.target;
+        setDistance(value);
+    }
 
 
     let content;
@@ -13,7 +30,27 @@ const IntroImage = () => {
                 <p className='titleText'>Find basketball courts around you, and
                     interact with other hoopers about the courts and the area.
                 </p>
-                <h2 className='title2'>Get Started</h2>
+                <h2 className='title2'>Get Started Finding Courts</h2>
+                <button className='currentLocation' onClick={useCurrent}>Use Current Location</button>
+                <div className='form-group'>
+                    <label for="address">Address</label>
+                    <input type="text" id="address" name="address" value={addressText} onChange={changeAddress}></input>
+                </div>
+                <div className='form-group'>
+                    <label for = "distance">Distance in Km</label>
+                    <input type="number" id = "distance" name = "distance" value={distance} onChange={changeDistance}></input>
+                </div>
+                <p>Type of Court</p>
+                <input type="radio" id="AnyCourt" name="anycourt" value="any"></input>
+                <label for="AnyCourt">Any Court</label>
+  
+                <input type="radio" id="indoor" name="indoor" value="indoor"></input>
+                <label for="indoor">Indoor Only</label>
+  
+                <input type="radio" id="outdoor" name="outdoor" value="outdoor"></input>
+                <label for="outdoor">Outdoor Only</label>
+                <button className='submission'>Find Nearby Courts</button>
+
 
 
 
