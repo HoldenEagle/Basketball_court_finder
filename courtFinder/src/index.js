cf06a46771e71691b56df0ba9d80e8446b70331c
Holden Eagle <holden.eagle@gmail.com>
2024-07-23 17:11:46 -0400

    delete app componetn

--- courtFinder/src/index.js
+++ courtFinder/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import IntroImage from './beginning';
 
-const App = () => <h1>Hello, React!</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
