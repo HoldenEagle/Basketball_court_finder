32cc4560f19aec048d1c1a908ef9eff34c3bc590
Holden Eagle <holden.eagle@gmail.com>
2024-07-31 22:37:07 -0400

    for privacy

--- courtFinder/src/beginning.js
+++ courtFinder/src/beginning.js
@@ -145,7 +145,7 @@ const IntroImage = () => {
             }
 
             const script = document.createElement('script');
-            script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyD_fO67awes0nJ9orpfk5VMkaH118EZfKU&libraries=places";
+            script.src = "Your google api key";
             script.id = 'google-maps-script';
             script.async = true;
             script.onload = resolve;
